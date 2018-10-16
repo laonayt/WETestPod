53a750f29051325cfce11e32c06a961aa460d949
mawei <mawei@zonekey.com.cn>
2018-10-16 11:14:24 +0800

    Dddd

---
+++ Classes/Temp.h
@@ -0,0 +1,13 @@
+//
+//  Temp.h
+//  CloudPlatform
+//
+//  Created by WE on 2018/10/16.
+//  Copyright © 2018年 zonekey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Temp : NSObject
+
+@end
---
+++ Classes/Temp.m
@@ -0,0 +1,13 @@
+//
+//  Temp.m
+//  CloudPlatform
+//
+//  Created by WE on 2018/10/16.
+//  Copyright © 2018年 zonekey. All rights reserved.
+//
+
+#import "Temp.h"
+
+@implementation Temp
+
+@end
